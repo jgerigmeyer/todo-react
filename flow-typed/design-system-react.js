0fe05750a5dc4e64bf704ee3af73bb516999beb4
Jonny Gerig Meyer <jonny@oddbird.net>
2018-07-10 16:54:59 -0400

    provide type definitions for design-system-react

---
+++ flow-typed/design-system-react.js
@@ -0,0 +1,11 @@
+declare module '@salesforce/design-system-react/components/button' {
+  import typeof { Component } from 'react';
+
+  declare export default Component;
+}
+
+declare module '@salesforce/design-system-react/components/input' {
+  import typeof { Component } from 'react';
+
+  declare export default Component;
+}
